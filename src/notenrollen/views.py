56123196fe61f65eb1700f9dcc0e40500cfd916a
EsGeh <SamuelGfroerer@googlemail.com>
2017-07-10 01:24:06 +0200

    removed some redundant code

--- src/notenrollen/views.py
+++ src/notenrollen/views.py
@@ -7,9 +7,6 @@ from django.shortcuts import render
 from .utils import database
 
 
-xslt_path = join(XSLT_DIR, "gen_catalogue_html.xslt")
-
-
 def index_page(request, **args):
     context = {}
     return render(request, "index.html", context )
@@ -25,67 +22,11 @@ def explore(request, **args):
     source_xml = etree.XML( "<root>" + xml_data + "</root>" )
 
     entry = {}
-    for xml_object in source_xml:
+    if len(source_xml) == 0:
+        raise( "object not found in database" )
 
-        title = xml_object.find("descriptiveMetadata/title")
-        if title is not None:
-            title = title.text
-        else:
-            title = "unknown"
-
-        type = xml_object.find("descriptiveMetadata/objectType")
-        if type is not None:
-            type = type.text
-        else:
-            type = "unknown"
-
-        instrument = xml_object.find("descriptiveMetadata/instrument")
-        if instrument is not None:
-            instrument = instrument.text
-        else: instrument = "unknown"
-
-        description = xml_object.find("descriptiveMetadata/objectDescription")
-        strDescription = ""
-        if description is not None:
-            for term in description:
-                strDescription = strDescription + term.text + "\n"
-        else: strDescription = "unknown"
-
-        actors = xml_object.find("actors")
-        strHersteller = ""
-        strKomponist = ""
-        strInterpret = ""
-        if actors is not None:
-            for actor in actors:
-                if(actor.tag == "Hersteller"):
-                    strHersteller = strHersteller + actor.text + "\n"
-                if(actor.tag == "Komponist"):
-                    strKomponist = strKomponist + actor.text + "\n"
-                if(actor.tag == "Interpret"):
-                    strInterpret = strInterpret + actor.text + "\n"
-
-            if strHersteller == "":
-                strHersteller = "unknown"
-            if strKomponist == "":
-                strKomponist = "unknown"
-            if strInterpret == "":
-                strInterpret = "unknown"
-
-        objectData = xml_object.find("objectData")
-        images = []
-        for image in objectData:
-            images.append(image.text)
-
-        entry["objectID"] = objectID
-        entry["title"] = title
-        entry["type"] = type
-        entry["instrument"] = instrument
-        entry["description"] = strDescription
-        entry["hersteller"] = strHersteller
-        entry["komponist"] = strKomponist
-        entry["interpret"] = strInterpret
-        entry["images"] = images
-
+    xml_object = source_xml[0]
+    entry = xml_object_to_python_dict(xml_object)
 
     context = {
         "entry": entry
@@ -106,55 +47,19 @@ def search(request, **args):
         # print( "keyword: {}".format( keyword ) )
         xml_data = database.search( keyword )
 
-    # print( "xml response:" )
-    # print( xml_data )
-
     # xml to python:
     from lxml import etree
     # the dummy "root" element is necessary for valid xml:
     source_xml = etree.XML( "<root>" + xml_data + "</root>" )
     search_entries = []
     for xml_object in source_xml:
-        entry = {}
-	
-        objectID = xml_object.find("descriptiveMetadata/objectID")
-        if objectID is not None:
-            objectID = objectID.text
-        else:
-            objectID = "unknown"
-        title = xml_object.find("descriptiveMetadata/title")
-        if title is not None:
-            title = title.text
-        else:
-            title = "unknown"
-        instrument = xml_object.find("descriptiveMetadata/instrument")
-        if instrument is not None:
-            instrument = instrument.text
-        else: instrument = "unknown"
-        composer = xml_object.find("actors/Komponist")
-        if composer is not None:
-            composer = composer.text
-        else: composer = "unknown"
-        interpreter = xml_object.find("actors/Interpret")
-        if interpreter is not None:
-            interpreter = interpreter.text
-        else: interpreter = "unknown"
-
-        entry["objectID"] = objectID
-        entry["title"] = title
-        entry["instrument"] = instrument
-        entry["composer"] = composer
-        entry["interpreter"] = interpreter
+        entry = xml_object_to_python_dict(xml_object)
         search_entries.append( entry )
 
     context = {
         "search_entries": search_entries
     }
     return render(request, "search.html", context )
-
-def quiz(request, **args):
-    context = {}
-    return render(request, "quiz.html", context )
 
 
 # rest api to ask for search requests:
@@ -163,21 +68,90 @@ def search_database(request, **args):
     xmldata=database.search(keyword)
     return HttpResponse(xmldata, content_type='application/xml')
 
-def gen_catalogue(request, **args):
+################################################
+## utils:
+################################################
+
+def xml_object_to_python_dict(xml_object):
     from lxml import etree
-    import requests as httplib
+    entry = {}
+    
+    objectID = xml_object.find("descriptiveMetadata/objectID")
+    if objectID is not None:
+        strObjectID = objectID.text
+    else:
+        strObjectID = "unknown"
+    title = xml_object.find("descriptiveMetadata/title")
+    if title is not None:
+        strTitle = title.text
+    else:
+        strTitle = "unknown"
+    instrument = xml_object.find("descriptiveMetadata/instrument")
+    if instrument is not None:
+        strInstrument = instrument.text
+    else: strInstrument = "unknown"
+    composer = xml_object.find("actors/Komponist")
+    if composer is not None:
+        strComposer = composer.text
+    else: strComposer = "unknown"
+    interpreter = xml_object.find("actors/Interpret")
+    if interpreter is not None:
+        strInterpreter = interpreter.text
+    else: strInterpreter = "unknown"
 
-    transformation = etree.XSLT(etree.parse(xslt_path))
-    url = basex_uri + "/rest/notenrollen/all_objects.xml"
-    headers={"Authorization": "Basic YWRtaW46YWRtaW4=", "request": "/" }
-    response = httplib.get(url=url, headers=headers)
-    source_xml = etree.XML( response.text )
+    type = xml_object.find("descriptiveMetadata/objectType")
+    if type is not None:
+        strType = type.text
+    else:
+        strType = "unknown"
 
-    # source_xml = etree.parse(cataloge_index_path)
-    catalog_html = transformation(source_xml, **args)
+    description = xml_object.find("descriptiveMetadata/objectDescription")
+    strDescription = ""
+    if description is not None:
+        for term in description:
+            if term.text is not None:
+                strDescription = strDescription + term.text + "\n"
+            else:
+                print( "WARNING: empty text field for object {}!".format( strObjectID ) )
+                print( etree.tostring( xml_object, pretty_print=True, encoding="unicode") )
+    else: strDescription = "unknown"
 
-    context = {
-        'catalog_html' : catalog_html
-    }
+    actors = xml_object.find("actors")
+    strHersteller = ""
+    strKomponist = ""
+    strInterpret = ""
+    if actors is not None:
+        for actor in actors:
+            if(actor.tag == "Hersteller"):
+                strHersteller = strHersteller + actor.text + "\n"
+            if(actor.tag == "Komponist"):
+                strKomponist = strKomponist + actor.text + "\n"
+            if(actor.tag == "Interpret"):
+                strInterpret = strInterpret + actor.text + "\n"
 
-    return render(request, "main.html", context )
+        if strHersteller == "":
+            strHersteller = "unknown"
+        if strKomponist == "":
+            strKomponist = "unknown"
+        if strInterpret == "":
+            strInterpret = "unknown"
+
+    objectData = xml_object.find("objectData")
+    images = []
+    for image in objectData:
+        images.append(image.text)
+
+    entry["objectID"] = strObjectID
+    entry["title"] = strTitle
+    entry["instrument"] = strInstrument
+    entry["composer"] = strKomponist
+    # entry["composer"] = composer
+    entry["interpreter"] = strInterpret
+    # entry["interpreter"] = interpreter
+
+    entry["type"] = strType
+    entry["description"] = strDescription
+    entry["hersteller"] = strHersteller
+    entry["images"] = images
+
+    return entry
